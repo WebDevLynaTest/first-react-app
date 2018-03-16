bc86f21648e3a617ad603793b695a66629b6064f
Graphic <graphic@SOD-SJH-603-05M.local>
2018-03-15 20:59:54 -0400

    Moving child p tags within parent tag. React only accepts ONE parent

--- src/components/paragraph/paragraph.js
+++ src/components/paragraph/paragraph.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './paragraph.css'
 
 export const Paragraph = () => (
-  <p className="p1">This is Paragraph 1 in red</p>
-  <p className="p2">This is Paragraph 2 in blue</p>
-  <p className="p3">This is Paragraph 3 in pink</p>
+  <div>
+    <p className="p1">This is Paragraph 1 in red</p>
+    <p className="p2">This is Paragraph 2 in blue</p>
+    <p className="p3">This is Paragraph 3 in pink</p>
+  </div>
 );
